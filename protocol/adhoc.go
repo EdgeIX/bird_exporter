dd687657fe74a83777a51e95c9087dd5e347861e
James Di Trapani <51264648+jamesditrapani@users.noreply.github.com>
2021-05-24 09:32:33 +0000

    Add Further Communities

--- protocol/adhoc.go
+++ protocol/adhoc.go
@@ -25,12 +25,49 @@ func GetLargeCommunities() []LargeCommunity {
                         First:  1101,
                         Last: 13,
                 },
-		LargeCommunity{
-			Name: "IRRDB Filtered Strict",
-			ASN: EdgeIX,
-			First: 1001,
-			Last: 1001,
-		},
+                LargeCommunity{
+                       Name: "RPKI Valid",
+                       ASN: EdgeIX,
+                       First: 1000,
+                       Last: 1,
+                },
+                LargeCommunity{
+                       Name: "RPKI Unknown",
+                       ASN: EdgeIX,
+                       First: 1000,
+                       Last: 2,
+                },
+                LargeCommunity{
+                       Name: "IRRDB Filtered Loose",
+                       ASN: EdgeIX,
+                       First: 1001,
+                       Last: 1000,
+                },
+                LargeCommunity{
+                       Name: "IRRDB Filtered Strict",
+                       ASN: EdgeIX,
+                       First: 1001,
+                       Last: 1001,
+                },
+                LargeCommunity{
+                       Name: "IRRDB Prefix Filtered",
+                       ASN: EdgeIX,
+                       First: 1101,
+                       Last: 9,
+                },
+                LargeCommunity{
+                       Name: "IRRDB Origin AS Filtered",
+                       ASN: EdgeIX,
+                       First: 1101,
+                       Last: 10,
+                },
+                LargeCommunity{
+                       Name: "Prefix not in Origin AS",
+                       ASN: EdgeIX,
+                       First: 1101,
+                       Last: 11,
+                },
         }
 	return LargeCommunities
 }
+
